0a33f22135eed9cecd7353928adc7c4147af5a94
Adam Smith <asmith1924@Adams-MacBook-Pro-2.local>
2020-11-21 12:09:12 -0800

    messed with skateboard controls

--- Player/playerskateboard/PlayerSkateBoard.gd
+++ Player/playerskateboard/PlayerSkateBoard.gd
@@ -11,14 +11,14 @@ func get_input():
 	velocity = Vector2()
 	
 	if Input.is_action_pressed('right_%s' % id):
-		velocity.x += 1.25
+		velocity.x += 1.5
 #	NTS: Pressing Left Causes Player To Stop Moving
 #	if Input.is_action_pressed('left_%s' % id):
-#		velocity.x -= .75
+#		velocity.x += 1
 	if Input.is_action_pressed('down_%s' % id):
-		velocity.y += 1
+		velocity.y += 1.25
 	if Input.is_action_pressed('up_%s' % id):
-		velocity.y -= 1
+		velocity.y -= 1.25
 	velocity = velocity.normalized() * speed
 
 func _physics_process(delta):
