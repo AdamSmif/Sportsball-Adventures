1e13a5709227896df9197106525dfc5a49c693f8
AdamSmif <35468207+AdamSmif@users.noreply.github.com>
2020-12-07 20:51:09 -0800

    fixed gronkbot bug

--- Enemies+Allies/Gronkobot/Gronkobot.gd
+++ Enemies+Allies/Gronkobot/Gronkobot.gd
@@ -1,39 +1,29 @@
 extends KinematicBody2D
 
-# Player Number
-# export var id = 2
-
-# Movement
 var velocity = Vector2()
+#-1 move left +1 move right
 export var direction = -1
+# true does not fall on cliff, false falls off cliff
 export var detects_cliffs = true
 
-# allows rigid bodies to stay rigid
-export (int, 0, 200) var push = 5   
-
 func _ready():
+	#if moving right move sprite to the right
 	if direction == 1:
 		$Sprite.flip_h = true
-#	$floor_checker.position.x = $CollisionShape2D.shape.get_extents().x * direction
-#	$floor_checker.enabled = detects_cliffs
-
-func _physics_process(_delta):
+	$floor_checker.position.x = $CollisionShape2D.shape.get_extents().x * direction
+	$floor_checker.enabled = detects_cliffs
 	
-# put in betwee and and is on floor once we know the bug solution
-#or not $floor_checker.is_colliding() and detects_cliffs 
-	if is_on_wall() and is_on_floor():
+func _physics_process(delta):
+	
+	# moves if bumps into wall
+	if is_on_wall() or not $floor_checker.is_colliding() and detects_cliffs and is_on_floor():
 		direction = direction * -1
 		$Sprite.flip_h = not $Sprite.flip_h
-#		$floor_checker.position.x = $CollisionShape2D.shape.get_extents().x * direction
+		$floor_checker.position.x = $CollisionShape2D.shape.get_extents().x * direction 
 	
-	velocity.y += 20
+	#gravity
+	velocity.y += 5
 	
-	velocity.x = -50 * direction
-	
+	velocity.x = 50 * direction
 	velocity = move_and_slide(velocity, Vector2.UP)
-
-
-
-
-
-
+	
