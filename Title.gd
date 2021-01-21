3ac9c8fb1335bcc381285af8eb3b4061e273a00c
AdamSmif <35468207+AdamSmif@users.noreply.github.com>
2021-01-20 20:10:01 -0800

    added smooth title screen

---
+++ Title.gd
@@ -0,0 +1,11 @@
+extends Control
+
+func _ready():
+
+	yield(get_tree().create_timer(1), "timeout")
+	$AnimationPlayer.play("fade")
+	yield(get_tree().create_timer(5), "timeout")
+	$AnimationPlayer.play_backwards("fade")
+	yield(get_tree().create_timer(3), "timeout")
+	get_tree().change_scene("res://UI/TitleScreen.tscn")
+	pass # Replace with function body
