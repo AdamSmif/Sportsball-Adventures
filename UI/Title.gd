9402e93cc29dae5d26b3ef5d3a2501e08a4c128b
Adam Smith <asmith1924@Adams-MacBook-Pro-2.local>
2021-02-02 21:32:30 -0800

    shortedn title intro

--- UI/Title.gd
+++ UI/Title.gd
@@ -2,10 +2,10 @@ extends Control
 
 func _ready():
 
-	yield(get_tree().create_timer(1), "timeout")
+	yield(get_tree().create_timer(2), "timeout")
 	$AnimationPlayer.play("fade")
 	yield(get_tree().create_timer(2), "timeout")
 	$AnimationPlayer.play_backwards("fade")
-	yield(get_tree().create_timer(3), "timeout")
+	yield(get_tree().create_timer(2), "timeout")
 	get_tree().change_scene("res://UI/TitleScreen.tscn")
 	pass # Replace with function body
