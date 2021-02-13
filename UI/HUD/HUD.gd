54fac58b429630336843176100d5c3ee25ed9006
AdamSmif <35468207+AdamSmif@users.noreply.github.com>
2021-02-13 14:25:05 -0800

    Revert "added health UI + removed hit sound effect"
    This reverts commit 7d04aefc3526e31e0e1975e63b988db10324ea9c.

--- UI/HUD/HUD.gd
+++ UI/HUD/HUD.gd
@@ -4,24 +4,8 @@ var bottles = 0
 # Next Level For Bottle Colelcting Levels
 export(String, FILE, "*.tscn") var bottle_world_scene
 
-var hearts = 4 setget set_hearts
-var max_hearts = 4 setget set_max_hearts
-
-onready var label = $HealthUI
-
-func set_hearts(value):
-	hearts = clamp(value, 0, max_hearts)
-	if label != null:
-		label.text = str(hearts)
-	
-func set_max_hearts(value):
-	max_hearts = max(value, 1)
-
 func _ready():
 	$Bottles.text = String(bottles)
-	self.max_hearts = PlayerStats.max_health
-	self.hearts = PlayerStats.health
-	PlayerStats.connect("health_changed", self, "set_hearts")
 
 func _physics_process(delta):
 		if bottles == 8:
