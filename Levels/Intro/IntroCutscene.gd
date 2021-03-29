a07dd7238541326236344eb9d8c1c9bd561bf47f
AdamSmif <35468207+AdamSmif@users.noreply.github.com>
2021-03-29 17:01:11 -0600

    code boys!

---
+++ Levels/Intro/IntroCutscene.gd
@@ -0,0 +1,14 @@
+extends Control
+
+export(String, FILE, "*.tscn") var next_world_scene
+
+func _ready():
+	yield(get_tree().create_timer(1), "timeout")
+	$AnimationPlayer.play("textfade")
+	yield(get_tree().create_timer(1), "timeout")
+#	get_tree().change_scene(next_world_scene)
+
+
+func _process(delta):
+	if Input.is_action_just_pressed("ui_accept"):
+		get_tree().change_scene(next_world_scene)
