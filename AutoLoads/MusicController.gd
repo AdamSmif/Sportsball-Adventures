d3e2a6047adc1050827e70cbdd2b8cb52bfe6d1a
AdamSmif <35468207+AdamSmif@users.noreply.github.com>
2021-04-07 10:59:25 -0600

    updated 3d part with music

--- AutoLoads/MusicController.gd
+++ AutoLoads/MusicController.gd
@@ -3,6 +3,7 @@ extends Node
 var rain_music = load("res://Music/RainShowersRVX2406.ogg")
 var menu_music = load("res://Music/the_past_8_bit_lofi_.ogg")
 var credits_music = load("res://Music/8_bit_cold_lake_lofi.ogg")
+var synth_music = load("res://Music/Dreamstate.ogg")
 
 func _ready():
 	pass
@@ -31,3 +32,10 @@ func stop_credits_music():
 	$Music.stream = credits_music
 	$Music.stop()
 	
+func play_synth_music():
+	$Music.stream = synth_music
+	$Music.play()
+	
+func stop_synth_music():
+	$Music.stream = synth_music
+	$Music.stop()
---
+++ Levels/3Dtest/3DPlatformTest.gd
@@ -0,0 +1,12 @@
+extends Spatial
+
+export(String, FILE, "*.tscn") var next_world_scene
+
+func _ready():
+	MusicController.play_synth_music()
+	yield(get_tree().create_timer(120), "timeout")
+	get_tree().change_scene(next_world_scene)
+	
+#func _process(delta):
+#	if Input.is_action_just_pressed("ui_accept"):
+#		get_tree().change_scene(next_world_scene)
